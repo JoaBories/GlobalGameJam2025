9921cc2d69fedb0482fccd0d982d8ad413670c95
Baptiste-Verdier <baptiste.verdier@artfx.fr>
2025-01-25 14:27:31 +0100

    Put UI images

--- Assets/inDev/Baptiste/UI_Script.cs
+++ Assets/inDev/Baptiste/UI_Script.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Script : MonoBehaviour
 {
+    public Button play;
+    public Button mainMenu;
+    public Button resume;
+    public Button quit;
+    public Button upgrade;
     // Start is called before the first frame update
     void Start()
     {
-        
+        play.GetComponent<Button>().onClick.AddListener(Play);
+        mainMenu.GetComponent<Button>().onClick.AddListener(MainMenu);
+        resume.GetComponent<Button>().onClick.AddListener(Resume);
+        quit.GetComponent<Button>().onClick.AddListener(Quit);
+        upgrade.GetComponent<Button>().onClick.AddListener(Upgrade);
     }
 
     // Update is called once per frame
@@ -15,4 +28,11 @@ public class UI_Script : MonoBehaviour
     {
         
     }
+    private void Play() { SceneManager.LoadScene(1); }
+    private void MainMenu() { SceneManager.LoadScene(0); }
+    private void Resume() { }
+    private void Quit() { Application.Quit(); }
+    private void Upgrade() { }
+
+
 }
